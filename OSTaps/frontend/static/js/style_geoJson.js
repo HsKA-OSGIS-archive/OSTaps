d20670ee0fc70977a369c42d2ee28967428efbda
lukas <l.alex.w@web.de>
2019-01-28 16:41:37 +0100

    completed legend

--- OSTaps/frontend/static/js/style_geoJson.js
+++ OSTaps/frontend/static/js/style_geoJson.js
@@ -22,7 +22,6 @@ var colorCode = "OrRd"
 var arrAttribute = "";
 var attributeName = "";
 
-
 // everything is based on this styleGeoJSON function
 // INPUTS:
 // - geoJSON2style: the L.geoJson object
@@ -41,6 +40,9 @@ function styleGeoJSON(geoJSON2style) {
 			fillOpacity: 1
 		});
 	});
+	if (arrAttribute != ""){
+		createChoroplethLegend();
+	}
 }
 
 // call the startup function when the document has finished loading:
@@ -60,19 +62,12 @@ function startup() {
 	s_select_fill_color = document.querySelector("#s_select_color");
 	s_select_numb_class = document.querySelector("#s_select_classes");
 
-
-
-
-
 	// overwrites the default color by a nicer color defined by us (outlineColor)
 	i_select_line_color.value = outlineColor;
 	f_select_line_width.value = lineWidth;
 	s_select_line_style.value = "solid";
 	s_select_fill_color.value = colorCode;
 	s_select_numb_class.value = numClasses;
-
-
-
 
 	// add listener to check if color is changed by the user
 	i_select_line_color.addEventListener("change", updateOutlineColor, true);
@@ -104,6 +99,8 @@ function updateOutlineColor(event) {
 function updateLineWidth(event) {
 	// get lineWidth
 	lineWidth = f_select_line_width.value;
+	console.log(f_select_line_width.value);
+	console.log(lineWidth)
 	styleGeoJSON(LeafletGeoJSON);
 }
 
@@ -116,17 +113,13 @@ function updateLineStyle(event) {
 	if (style == "solid"){
 		outlineStyle = ssolid;
 	}
-
 	if (style == "dashed"){
 	outlineStyle = sdashed;
 	}
-
 	if (style == "dotted"){
 	outlineStyle = sdotted;
 	}
-
 	styleGeoJSON(LeafletGeoJSON);
-
 }
 
 // FILL COLOR
@@ -143,6 +136,7 @@ function updateNumclasses(event) {
 	styleGeoJSON(LeafletGeoJSON);
 }
 
+var colorBrew;
 function getColor(feature){
 	if (arrAttribute == ""){
 		return fillColor;
@@ -150,10 +144,38 @@ function getColor(feature){
 		t = new Color();
 		t.setNumClasses(numClasses);
 		t.setColorCode(colorCode);
-
 		t.setSeries(arrAttribute)
 		t.classify(arrAttribute);
-
+		colorBrew = t;
 		return t.getColorInRange(feature.feature.properties[attributeName])
 	}
+}
+
+var legend;
+// create Legend:
+function createChoroplethLegend(){
+	if (typeof legend === 'object') {
+		map.removeControl(legend); // removes old legend
+	}
+
+	legend = L.control({position: 'bottomright'});
+
+	legend.onAdd = function (map) {
+		var colors = colorBrew.getColors();
+		var range = colorBrew.range
+		var div = L.DomUtil.create('div', 'info legend'),
+			grades = range,
+			labels = [],
+			from, to;
+		for (var i = 0; i < grades.length; i++) {
+			from = grades[i];
+			to = grades[i + 1];
+			if (to) {
+				labels.push('<i style="background:' + colors[i] + '"></i> ' + from + " - " + to);
+			}
+		}
+		div.innerHTML = labels.join('<br>');
+		return div;
+	};
+	legend.addTo(map);
 }
